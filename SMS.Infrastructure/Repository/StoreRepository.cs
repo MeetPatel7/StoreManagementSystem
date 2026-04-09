d1c32b752510e935db45c92729f65c3bb8599def
Meet Patel <meetpatel1865@gmail.com>
2026-04-09 09:56:56 +0530

    getstore api commit

---
+++ SMS.Infrastructure/Repository/StoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Infrastructure.Repository
+{
+    public class StoreRepository
+    {
+        public StoreRepository() { }
+
+
+
+
+    }
+}
